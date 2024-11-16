2b574e10cfbb1116acfcf3358bcf8158c93b6eba
SunkanmiAyeni <jumokeoayeni@gmail.com>
2024-11-16 13:49:39 +0100

    BPC

---
+++ break.py
@@ -0,0 +1,7 @@
+word=input("Please enter a letter")
+for i in word:
+    if i=='A':
+        print("A has been found")
+        break
+    else:
+        print("A not found")
---
+++ pass.py
@@ -0,0 +1,11 @@
+for i in range(10):
+    if i % 3==0:
+        print ("Ronaldo")
+    elif i % 5==0:
+        print("Suiiiii")
+    elif i% 7==0:
+        print("Man city")
+    elif i% 9==0:
+        print ("Kdb")
+    else:
+        print ("L you")
